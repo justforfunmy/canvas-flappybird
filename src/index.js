f2a1c3ffc44d64b20cbcd5d78549c43e234d64b7
tangkaiqiang <justforgit@163.com>
2020-08-27 13:35:03 +0800

    update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const CANVAS_HEIGHT = 400;
 
 const BAMBOO_WIDTH = 40;
 const BAMBOO_GAP = 120;
-const BAMBOO_SPEED = 256;
+const BAMBOO_SPEED = 150;
 
 const X_GAP = 160;
 
@@ -24,13 +24,15 @@ canvas.style.background = '#fff';
 canvas.style.border = '1px solid grey';
 root.appendChild(canvas);
 
+// variables
 let started = false;
+let startNumber = 4;
 
 // game objects
 
 const hero = {
-  speed: 256,
-  x: 0,
+  speed: 100,
+  x: 80,
   y: canvas.height / 2
 };
 
@@ -53,11 +55,25 @@ function Bamboo(x) {
 // player input
 
 const keysDown = {};
+let jump = false;
+let timer = null;
 
 addEventListener(
   'keydown',
   (e) => {
-    keysDown[e.keyCode] = true;
+    if (e.keyCode === 38) {
+      if (jump) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+          jump = false;
+        }, 250);
+      } else {
+        jump = true;
+        timer = setTimeout(() => {
+          jump = false;
+        }, 250);
+      }
+    }
   },
   false
 );
@@ -66,7 +82,7 @@ addEventListener(
   'keyup',
   (e) => {
     if (e.keyCode === 32) {
-      started = !started;
+      started = true;
     }
     delete keysDown[e.keyCode];
   },
@@ -76,21 +92,13 @@ addEventListener(
 // update projects
 
 const updateHero = (modifier) => {
-  if (38 in keysDown) {
-    // Player holding up
-    hero.y -= hero.speed * modifier;
+  if (!started || startNumber > 0) {
+    return;
   }
-  if (40 in keysDown) {
-    // Player holding down
+  if (jump) {
+    hero.y -= hero.speed * 2 * modifier;
+  } else {
     hero.y += hero.speed * modifier;
-  }
-  if (37 in keysDown) {
-    // Player holding left
-    hero.x -= hero.speed * modifier;
-  }
-  if (39 in keysDown) {
-    // Player holding right
-    hero.x += hero.speed * modifier;
   }
 };
 
@@ -109,6 +117,15 @@ const updateBamboo = (modifier) => {
   });
 };
 
+let current = Date.now();
+const updateNumber = () => {
+  const now = Date.now();
+  if (now - current > 500) {
+    startNumber -= 1;
+    current = now;
+  }
+};
+
 // draw bamboo
 
 const drawBamboo = (item) => {
@@ -119,17 +136,50 @@ const drawBamboo = (item) => {
   ctx.fillRect(x, upHeight + BAMBOO_GAP, width, downHeight);
 };
 
+// draw text
+
+const drawText = () => {
+  if (startNumber > 0) {
+    ctx.fillStyle = 'rgba(255,255,255,0.5)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  ctx.fillStyle = '#111';
+  ctx.strokeStyle = '#111'; // 设置笔触的颜色
+  ctx.font = "bold 40px '字体','字体','微软雅黑','宋体'"; // 设置字体
+  ctx.textBaseline = 'hanging'; // 在绘制文本时使用的当前文本基线
+  ctx.textAlign = 'center';
+  if (startNumber > 0 && startNumber < 4) {
+    ctx.fillText(startNumber, canvas.width / 2, canvas.height / 2 - 30);
+  } else if (startNumber === 4) {
+    ctx.fillText('空格键开始', canvas.width / 2, canvas.height / 2 - 30);
+  }
+};
+
+// draw hero
+
+const drawHero = () => {
+  ctx.save();
+  if (heroReady) {
+    if (jump) {
+      ctx.rotate(-45);
+    }
+    ctx.drawImage(heroImage, hero.x, hero.y);
+  }
+  ctx.restore();
+};
+
 // render
 
 const render = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  if (heroReady) {
-    ctx.drawImage(heroImage, hero.x, hero.y);
-  }
+  drawHero();
 
   bambooArray.forEach((item) => {
     drawBamboo(item);
   });
+
+  drawText();
 };
 
 // The main game loop
@@ -141,6 +191,10 @@ const main = () => {
 
   updateHero(delta / 1000);
   if (started) {
+    updateNumber();
+  }
+
+  if (startNumber < 0) {
     updateBamboo(delta / 1000);
   }
 
@@ -152,9 +206,9 @@ const main = () => {
   requestAnimationFrame(main);
 };
 
-let startX = 50;
+let startX = 400;
 const init = () => {
-  while (startX < canvas.width) {
+  while (startX < canvas.width + 400) {
     const bamboo = new Bamboo(startX);
     bambooArray.push(bamboo);
     startX += X_GAP;
